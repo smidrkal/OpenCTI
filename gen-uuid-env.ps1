ca045c1007055ee15323d51faa45cd94bc0ad422
smidrkal <josef@smidrkal.cz>
2024-01-24 21:53:42 +0100

    fix: powershell uuid generator

--- gen-uuid-env.ps1
+++ gen-uuid-env.ps1
@@ -1,5 +1,18 @@
 ## generate UUIDs for new .env on Windows
 
-# gen UUID: powershell -Command "[guid]::NewGuid().ToString()"
+## if you don't want error '...not digitally signed. The script will not execute on the system.':
+# Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
-Get-Content -Path .\.env.template -replace ' # generate new for each var at https://www.uuidgenerator.net/', [guid]::NewGuid().ToString() > .\.env 
+# gen UUID: powershell -Command "[guid]::NewGuid().ToString()"
+## problematic. if working, all replaced with the same guid.
+#Get-Content -Path .\.env.template -replace ' # generate new for each var at https://www.uuidgenerator.net/', [guid]::NewGuid().ToString() > .\.env 
+
+## works, but provides one GUID on every line. We need different for each line.
+#(Get-Content ".env.template") -replace ' # generate new for each var at https://www.uuidgenerator.net/', [guid]::NewGuid().ToString() | Set-Content .env
+
+$lines = Get-Content ".env.template"
+$len = $lines.count 
+for($i=0;$i-lt$len;$i++){
+        $lines[$i] = $lines[$i] -replace ' # generate new for each var at https://www.uuidgenerator.net/', [guid]::NewGuid().ToString()
+}
+$lines > ".env"
